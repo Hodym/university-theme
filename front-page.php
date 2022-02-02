8d48d67f0227006e60629d245fd68dbf292a2ba4
Hodym <hodimchukdmitriy@gmail.com>
2022-02-02 03:44:39 +0200

    display custom date-time format

--- front-page.php
+++ front-page.php
@@ -27,8 +27,14 @@
 
         <div class="event-summary">
           <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
-            <span class="event-summary__month"><?php the_time('M'); ?></span>
-            <span class="event-summary__day"><?php the_time('d'); ?></span>
+            <span class="event-summary__month">
+              <?php
+                $eventDate = get_field('date_event');
+                $event_date = DateTime::createFromFormat('Y-m-d H:i:s', $eventDate);
+                echo $event_date->format('M');
+              ?>
+            </span>
+            <span class="event-summary__day"><?= $event_date->format('d'); ?></span>
           </a>
           <div class="event-summary__content">
             <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
