6b7354b7b76ac19f5542311ab8e7289dd486cdc8
Hodym <hodimchukdmitriy@gmail.com>
2022-02-06 20:21:24 +0200

    search js front-end

--- functions.php
+++ functions.php
@@ -143,23 +143,8 @@ add_action( 'widgets_init', 'world_university_widgets_init' );
 /**
  * Enqueue scripts and styles.
  */
-function world_university_scripts() {
-	wp_enqueue_style( 'world-university-style', get_stylesheet_uri(), array(), _S_VERSION );
-	wp_style_add_data( 'world-university-style', 'rtl', 'replace' );
+require get_template_directory() . '/inc/world-university-scripts.php';
 
-	wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
-  	wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
-
-
-	wp_enqueue_script('main-university-js', get_template_directory_uri() .'/js/scripts-bundled.js', NULL, _S_VERSION, true);
-	wp_enqueue_script( 'world-university-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
-	wp_enqueue_script( 'googleMap', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAEma_TDC70km86NbVjUcBH9odX9TaWOFE', NULL, _S_VERSION, true );
-
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
-}
-add_action( 'wp_enqueue_scripts', 'world_university_scripts' );
 
 /**
  * Implement the Custom Header feature.
